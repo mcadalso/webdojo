38e08b34f19e2bc241595b0bf7df6a9041089478
Miguel <miguel.simoes@artsoft.pt>
2025-11-27 09:55:42 +0000

    Atualização aula 78 - Bruno

--- api/index.js
+++ api/index.js
@@ -3,8 +3,8 @@ const app = express()
 const port = 3333
 
 app.get('/', (req, res) => {
-//  res.send('Hello Ninja do Cypress!!!')
-  res.json({message:'API do Curso Ninja do Cypress!'})
+//  res.send('Hello Ninja do Cypress!!')
+  res.json({message:'API do Curso Ninja do Cypress!!!'})
 })
 
 app.listen(port, () => {
